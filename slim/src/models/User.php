e3657b5793cfa8b2f0136e2c2006a304be6d307f
JuanBlasGonzalez <juan.gonzalez@alu.ing.unlp.edu.ar>
2026-04-28 19:05:16 -0300

    modelo user modificado

--- slim/src/models/User.php
+++ slim/src/models/User.php
@@ -3,13 +3,26 @@
 namespace App\models;
 
 use App\config\DB;
+use PDO;
 
 class User {
-    // Get all users from the database
-    public static function getAll()
-    {
+    public $id;
+    public $name;
+    public $email;
+    public $balance;
+
+    // Obtener todos los usuarios para el endpoint GET /users
+    public static function getAll() {
         $db = DB::getConnection();
-        $stmt = $db->query("SELECT * FROM usuario");
+        //Retornamos NOMBRE y VALOR del portfolio
+        $stmt = $db->query("SELECT id, name, balance FROM users"); 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Método para validar el password
+    public static function validarPassword($password) {
+        // Mínimo 8 caracteres, una mayúscula, una minúscula, un número y un especial
+        $regex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/";
+        return preg_match($regex, $password);
     }
 }
