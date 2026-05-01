b622823d1781995a4171a6d8f9313640e25c17c4
JuanBlasGonzalez <juan.gonzalez@alu.ing.unlp.edu.ar>
2026-05-01 04:57:14 -0300

    Endpoints y AuthController

---
+++ slim/src/controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\models\User;
+
+class AuthController {
+
+    public static function login(Request $request, Response $response) {
+       
+    }
+
+    public static function logout(Request $request, Response $response) {
+        
+    }
+}
